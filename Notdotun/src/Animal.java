c02a97448490a9c0640d7022dd0631b8e67ee6a5
mxrtins04 <olatunbosunmartins14@gmail.com>
2026-02-02 09:09:57 +0100

    Animal.java update

--- Notdotun/src/Animal.java
+++ Notdotun/src/Animal.java
@@ -2,4 +2,7 @@ public class Animal {
     public void makeSound(){
         System.out.print("Nels is making sound!!!");
     }
+    public void move(){
+        System.out.print("I'm moving boiiii!!");
+    }
 }
